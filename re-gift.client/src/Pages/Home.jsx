8521e176b38ea14807efb578f166388b345fe1ca
Parisa8209 <143500667+Parisa8209@users.noreply.github.com>
2024-09-05 13:09:14 +0200

    Resize Carousel and images

--- re-gift.client/src/Pages/Home.jsx
+++ re-gift.client/src/Pages/Home.jsx
@@ -1,24 +1,24 @@
-﻿import { Carousel } from 'react-bootstrap'; // Importera Carousel från React-Bootstrap
+﻿import { Carousel } from 'react-bootstrap'; // Import Carousel from React-Bootstrap
 import '../css/Home.css';
-import HM from '../img/HM.jpg'; // Din första bild
-import Nike from '../img/Nike.jpg'; // Din andra bild
-import Amazon from '../img/Amazon.jpg'; // Din tredje bild
+import HM from '../img/HM.jpg'; // Your first image
+import Nike1 from '../img/Nike1.jpg'; // Your second image
+import Amazon1 from '../img/Amazon1.jpg'; // Your third image
 
 export default function Home() {
     return (
         <div className="container py-4" id="home">
             <div className="bg-body-tertiary p-5 rounded">
                 <div className="col-sm-8 py-5 mx-auto">
-                    <h1 className="display-5 fw-normal">Välkommen till Regift Card hemsida</h1>
-                    <p className="lead">
+                    <h1 className="display-5 fw-normal smaller-heading">Välkommen till Regift Card hemsida</h1>
+                    <p className="lead smaller-text">
                         Här kan du köpa och sälja presentkort på ett enkelt och säkert sätt.
                         Oavsett om du vill bli av med ett oanvänt presentkort eller letar efter ett bra erbjudande på ett presentkort du vill ha, är vi här för att hjälpa dig!
                     </p>
                 </div>
             </div>
 
-            {/* Karusell */}
-            <Carousel className="my-5">
+            {/* Carousel */}
+            <Carousel className="custom-carousel my-5">
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
@@ -27,31 +27,28 @@ export default function Home() {
                     />
                     <Carousel.Caption>
                         <h3>HM</h3>
-                       {/* <p>Beskrivning av första bilden.</p>*/}
                     </Carousel.Caption>
                 </Carousel.Item>
 
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src={Nike}
-                        alt="Andra bilden"
+                        src={Nike1}
+                        alt="Nike"
                     />
                     <Carousel.Caption>
                         <h3>Nike</h3>
-                       {/* <p>Beskrivning av andra bilden.</p>*/}
                     </Carousel.Caption>
                 </Carousel.Item>
 
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src={Amazon}
-                        alt="Tredje bilden"
+                        src={Amazon1}
+                        alt="Amazon"
                     />
                     <Carousel.Caption>
                         <h3>Amazon</h3>
-                       {/* <p>Beskrivning av tredje bilden.</p>*/}
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
