ffdb80789309b5c6588c3f57d48e789b9e80d718
sasanrashidi <sasan.rashidi@edu.newton.se>
2024-09-14 18:11:25 +0200

    betalsätt och historia

---
+++ re-gift.client/src/Pages/PurchaseHistory.jsx
@@ -0,0 +1,31 @@
+﻿import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+export function PurchaseHistory() {
+    const { purchaseHistory } = useContext(AppContext);
+
+    return (
+        <div>
+            <h2>Köphistorik</h2>
+            {purchaseHistory.length === 0 ? (
+                <p>Ingen köphistorik tillgänglig.</p>
+            ) : (
+                <ul className="list-group">
+                    {purchaseHistory.map((purchase, index) => (
+                        <li key={index} className="list-group-item">
+                            <strong>Köpdatum:</strong> {purchase.date}
+                            <ul>
+                                {purchase.items.map((item, idx) => (
+                                    <li key={idx}>
+                                        {item.title} - {item.discountedPrice} Kr
+                                    </li>
+                                ))}
+                            </ul>
+                            <strong>Totalpris:</strong> {purchase.totalPrice} Kr
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+}
